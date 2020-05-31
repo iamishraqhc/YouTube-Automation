3404b55792d4ba548960c7dbfdd52cbd61fb87ca
Ishraq Haider Chowdhury <ishraq.h.c@gmail.com>
2020-05-31 08:14:25 +0600

    logged into youtube studio, need to write another script for logging in to browser

--- puppeteer_script.js
+++ puppeteer_script.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer-core');
+const identity = require('./user.json');
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -6,11 +7,21 @@ const puppeteer = require('puppeteer-core');
         'executablePath': 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
     });
     const page = await browser.newPage();
-    await page.goto("https://www.google.com");
+    await page.goto("https://studio.youtube.com");
     await page.waitFor(2000);
-    await page.type('.gLFyf.gsfi', 'puppeteer vs selenium');
+    // await page.type('.gLFyf.gsfi', 'puppeteer vs selenium');
+    // await page.waitFor(2000);
+    // await page.click('input.gNO89b');
+    await page.type('#identifierId', identity.identity.user);
     await page.waitFor(2000);
-    await page.click('input.gNO89b');
+    await page.click('#identifierNext');
     await page.waitFor(5000);
     await browser.close();
 })();
+
+/* 
+    There are issues to bypass google login with puppeteer so it will be kept as it is for a while. 
+    I will be writing another script using selenium and let's see how that goes.
+    If I find a solution with puppeteer, I will update this script.
+    For now, its marked as incomplete.
+*/
--- test_script.js
+++ test_script.js
@@ -3,19 +3,19 @@ By = webdriver.By,
 until = webdriver.until,
 Key = webdriver.Key;
 
-var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).forBrowser('chrome').build();
+var chrome = require('selenium-webdriver/chrome');
+var options = new chrome.Options();
+options.addArguments('--user-data-dir=C:\\Users\\Ishraq Haider\\AppData\\Local\\Google\\Chrome\\User Data\\Default');
+var driver = new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build();
+// var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).forBrowser('chrome').build();
 (async function google_search(){
-    await driver.get('https://www.google.com/');
-    await driver.findElement(By.name('q')).sendKeys('javaScript');
+    await driver.get('https://www.youtube.com');
+    // await driver.findElement(By.id('identifierId')).sendKeys('entrep.haider');
+    // await driver.sleep(1000);
+    // await driver.findElement(By.id('identifierNext')).click();
+    await driver.findElement(By.xpath('//*[@id="avatar-btn"]')).click();
     await driver.sleep(1000);
-    await driver.findElement(By.name('btnK')).click();
+    await driver.findElement(By.xpath('//*[@id="items"]/ytd-compact-link-renderer[3]')).click();
     await driver.sleep(5000);
     await driver.quit();
 })();
-
-/* 
-    There are issues to bypass google login with selenium so it will be kept as it is for a while. 
-    I will be writing another script using puppeteer and let's see how that goes.
-    If I find a solution with selenium webdriver, I will update this script.
-    For now, its marked as incomplete.
-*/
